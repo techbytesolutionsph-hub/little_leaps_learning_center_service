ec7bb8ec1e9ab24dc5ab574a3b015cc2053bb584
Glen Mark Anduiza <glenmark.anduiza@ibm.com>
2026-07-27 15:04:09 +0800

    Initial commit

---
+++ src/main/java/ph/com/lllc/LittleLeapsLearningCenterServiceApplication.java
@@ -0,0 +1,13 @@
+package ph.com.lllc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleLeapsLearningCenterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LittleLeapsLearningCenterServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ph/com/lllc/LittleLeapsLearningCenterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ph.com.lllc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleLeapsLearningCenterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
